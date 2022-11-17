47d2b9c56bb2efcb064b1a1505552ec8dcb38e4a
Anti6ot <kradenihs@gmail.com>
2022-11-17 20:43:31 +0500

    9-array task1 решения в 3х версиях

--- module-1/9-Array/task1/task1.js
+++ module-1/9-Array/task1/task1.js
@@ -1,24 +1,69 @@
-const peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария', 'Светлана', 'Артем', 'Глеб']
+// const peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария', 'Светлана', 'Артем', 'Глеб']
 
-function leaveQueueWithoutParcel(item) {
+// function leaveQueueWithoutParcel(item) {
+//
+//     for(let i = item.length; i > 0 ; i--){
+//         let nik = item.splice(0, 1)
+//     alert(`${nik} не получил(а) посылку и ушел(ла) из очереди”`)
+//     }
+// }
 
-    for(let i = item.length; i > 0 ; i--){
-        let nik = item.splice(0, 1)
-    alert(`${nik} не получил(а) посылку и ушел(ла) из очереди”`)
+//Раскоментируй код выше и поочерёдно раз/ком код ниже
+
+
+//--------------Вот Этот вариан решения мне понравился больше всего т.к. у юзера есть выбор и рекурсия это моя любимая тема---------------
+
+/*
+function given(arr) {
+    let hook = confirm('Отдать посылку?')
+    if(hook === true){
+        let array = arr.shift()
+        alert(`${array} получил(а) посылку. В очереди осталось ${arr.length} человек`)
+        return (given(arr))
+    }else {
+        leaveQueueWithoutParcel(arr)
     }
 }
 
-const giveParcel = (arr) => {
+given(peopleWaiting)
+*/
+
+
+
+//---------------Этот вариант просто с помощью цикла и условия выводит пользователей получ/не получ пос.--------
+
+/*const giveParcel = (arr) => {
     let array = []
-        return function splice(parm) {
-            if( parm === 'gift' && arr.length > 4) {
+        for(let i = 0; i < arr.length; i++){
+            if(arr.length > 4) {
                 let name = arr.splice(0, 1)
-                return (array = name,
-                    alert(`${name} получил(а) посылку. В очереди осталось ${arr.length} человек`))
+                array = name
+                alert(`${name} получил(а) посылку. В очереди осталось ${arr.length} человек`)
             } else {
                 leaveQueueWithoutParcel(arr)
             }
         }
+
+}
+
+giveParcel(peopleWaiting)
+console.log(peopleWaiting)*/
+
+
+
+//------------этот с параметром -----------------
+/*const giveParcel = (arr) => {
+    let array = []
+    return function (prop) {
+        if( prop === 'gift' && arr.length > 4) {
+            let name = arr.splice(0, 1)
+            return (array = name,
+                alert(`${name} получил(а) посылку. В очереди осталось ${arr.length} человек`))
+        } else {
+            leaveQueueWithoutParcel(arr)
+        }
+    }
+
 }
 
 const func = giveParcel(peopleWaiting)
@@ -28,5 +73,7 @@ func('gift')
 func('gift')
 func('gift')
 
-console.log(peopleWaiting)
+console.log(peopleWaiting)*/
+
+
 
