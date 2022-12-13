1015edc0ebc208f4494440ad32b9646ca7328eea
Anti6ot <kradenihs@gmail.com>
2022-12-13 16:54:14 +0500

    compleate DOM part 2 task1 and editing DOM part 1 task2

--- module-1/14-DOMpart1/task2/tasks-list.js
+++ module-1/14-DOMpart1/task2/tasks-list.js
@@ -16,59 +16,118 @@ const tasks = [
         text: 'Выполнить ДЗ после урока',
     },
 ]
-tasks.forEach( (value) => {
-    const taskItem = document.createElement('div')
-    taskItem.className = 'task-item'
+// version 1
+// tasks.forEach( (value) => {
+//     const taskItem = document.createElement('div')
+//     taskItem.className = 'task-item'
+//
+//     const taskItemMainContainer = document.createElement('div')
+//     taskItemMainContainer.className = 'task-item__main-container'
+//     taskItem.append(taskItemMainContainer)
+//
+//     const taskItemMainContent = document.createElement('div')
+//     taskItemMainContent.className = 'task-item__main-content'
+//     taskItemMainContainer.append(taskItemMainContent)
+//
+//     const checkboxForm = document.createElement('form')
+//     checkboxForm.className = 'checkbox-form'
+//     taskItemMainContent.append(checkboxForm)
+//
+//
+//     const input = document.createElement('input')
+//     input.className = 'checkbox-form__checkbox'
+//     input.type = 'checkbox'
+//
+//     const label = document.createElement('label')
+//
+//     const span = document.createElement('span')
+//     span.textContent = 'Посмотреть новый урок по JavaScript'
+//     span.className = 'task-item__text'
+//     taskItemMainContent.append(span)
+//
+//     const button = document.createElement('button')
+//
+//     button.className = 'task-item__delete-button default-button delete-button'
+//     button.textContent = 'Удалить'
+//     button.dataset.dataDeleteTaskId = '5'
+//     taskItemMainContainer.append(button)
+//
+//
+//     for(let key in value){
+//         if(key === 'id'){
+//             taskItem.dataset.dataTaskId = value[key]
+//             tasksList.append(taskItem)
+//
+//             input.id = value[key]
+//             checkboxForm.append(input)
+//
+//             label.htmlFor = value[key]
+//             checkboxForm.append(label)
+//
+//             span.textContent = value.text
+//
+//             if (value.completed){
+//                 input.checked = true
+//             }
+//         }
+//     }
+// })
 
-    const taskItemMainContainer = document.createElement('div')
-    taskItemMainContainer.className = 'task-item__main-container'
-    taskItem.append(taskItemMainContainer)
-
-    const taskItemMainContent = document.createElement('div')
-    taskItemMainContent.className = 'task-item__main-content'
-    taskItemMainContainer.append(taskItemMainContent)
-
-    const checkboxForm = document.createElement('form')
-    checkboxForm.className = 'checkbox-form'
-    taskItemMainContent.append(checkboxForm)
-
-
-    const input = document.createElement('input')
-    input.className = 'checkbox-form__checkbox'
-    input.type = 'checkbox'
-
-    const label = document.createElement('label')
-
-    const span = document.createElement('span')
-    span.textContent = 'Посмотреть новый урок по JavaScript'
-    span.className = 'task-item__text'
-    taskItemMainContent.append(span)
-
-    const button = document.createElement('button')
-
-    button.className = 'task-item__delete-button default-button delete-button'
-    button.textContent = 'Удалить'
-    button.dataset.dataDeleteTaskId = '5'
-    taskItemMainContainer.append(button)
-
-
-    for(let key in value){
-        if(key === 'id'){
-            taskItem.dataset.dataTaskId = value[key]
-            tasksList.append(taskItem)
-
-            input.id = value[key]
-            checkboxForm.append(input)
-
-            label.htmlFor = value[key]
-            checkboxForm.append(label)
-
-            span.textContent = value.text
-
-            if (value.completed){
-                input.checked = true
-            }
-        }
-    }
-})
-
+//new version
+// function taskAdd(taskId, taskTxt){
+//     const tasksList = document.querySelector('.tasks-list')
+//
+//     const taskItem = document.createElement('div')
+//     taskItem.className = 'task-item'
+//
+//     const taskItemMainContainer = document.createElement('div')
+//     taskItemMainContainer.className = 'task-item__main-container'
+//     taskItem.append(taskItemMainContainer)
+//
+//     const taskItemMainContent = document.createElement('div')
+//     taskItemMainContent.className = 'task-item__main-content'
+//     taskItemMainContainer.append(taskItemMainContent)
+//
+//     const checkboxForm = document.createElement('form')
+//     checkboxForm.className = 'checkbox-form'
+//     taskItemMainContent.append(checkboxForm)
+//
+//
+//     const input = document.createElement('input')
+//     input.className = 'checkbox-form__checkbox'
+//     input.type = 'checkbox'
+//
+//     const label = document.createElement('label')
+//
+//     const span = document.createElement('span')
+//     span.textContent = ''
+//     span.className = 'task-item__text'
+//     taskItemMainContent.append(span)
+//
+//     const button = document.createElement('button')
+//
+//     button.className = 'task-item__delete-button default-button delete-button'
+//     button.textContent = 'Удалить'
+//     button.dataset.dataDeleteTaskId = '5'
+//     taskItemMainContainer.append(button)
+//
+//     taskItem.dataset.dataTaskId = taskId
+//     tasksList.append(taskItem)
+//
+//     input.id = taskId
+//     checkboxForm.append(input)
+//
+//     label.htmlFor = taskId
+//     checkboxForm.append(label)
+//
+//     span.textContent = taskTxt
+//
+//
+//
+// }
+//
+//
+// tasks.forEach( (value) => {
+//
+//     taskAdd(value.id, value.text)
+// })
